39a084a22ff3308dc11395ce15864ce0d953be92
DESKTOP-UJO6H8P\luther <251179255@qq.com>
2020-05-07 22:06:24 +0800

    删除redis

--- Server/UnitTest/Tests/Test_redis.h
+++ Server/UnitTest/Tests/Test_redis.h
@@ -2,12 +2,17 @@
 #define Test_redis_h__
 
 #include "Solution.h"
-#include "hiredis.h"
+
 class Test_Redis : public Solution 
 {
 public:
 	void Excute() 
 	{
+		
+		
 	}
 };
+
+
+
 #endif // Test_redis_h__
